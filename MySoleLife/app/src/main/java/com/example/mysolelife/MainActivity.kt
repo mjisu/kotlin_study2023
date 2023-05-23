6826630587b9d1011f0ed11898999ac75d4ee55b
mjisu <91945214+mjisu@users.noreply.github.com>
2023-05-23 12:38:52 +0900

    Add files via upload

---
+++ MySoleLife/app/src/main/java/com/example/mysolelife/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mysolelife
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+
+            auth.signOut()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+
+        }
+    }
+}
